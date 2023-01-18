81c2338a581940c5dec2cd4ebac41343c064dead
PascalCross <pascal.cross@immutable.com>
2023-01-19 09:48:20 +1300

    DX-1630 tweaks to test script

--- imx-sdk-unity-project/Assets/Scripts/sdkTest.cs
+++ imx-sdk-unity-project/Assets/Scripts/sdkTest.cs
@@ -6,30 +6,33 @@ using api.Client;
 using api.Model;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class sdkTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         Configuration config = new Configuration();
         config.BasePath = "https://api.sandbox.x.immutable.com";
+        
+        // Testing asset lists
         var apiInstance = new AssetsApi(config);
+        
+        try
+        {
+            // Get a list of assets
+            ListAssetsResponse result = apiInstance.ListAssets();
+            Debug.Log(result.ToJson());
+        }
+        catch (ApiException  e)
+        {
+            Debug.Log("Exception when calling AssetsApi.ListAssets: " + e.Message);
+            Debug.Log("Status Code: " + e.ErrorCode);
+            Debug.Log(e.StackTrace);
+        }
+        
+        // Testing getting users
         var usersInstance = new UsersApi();
         
-        
-        // try
-        // {
-        //     // Get a list of assets
-        //     ListAssetsResponse result = apiInstance.ListAssets();
-        //     Debug.Log(result.ToJson());
-        // }
-        // catch (ApiException  e)
-        // {
-        //     Debug.Log("Exception when calling AssetsApi.ListAssets: " + e.Message);
-        //     Debug.Log("Status Code: " + e.ErrorCode);
-        //     Debug.Log(e.StackTrace);
-        // }
-
         try
         {
             GetUsersApiResponse result = usersInstance.GetUsers("0x7b680319568a55bf60e94291cDf4E939d603cE96");
