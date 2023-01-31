65407a527f92e7fe36f66ec5febe0aa927321c0f
Praveen Suluvai <praveen.suluvai@immutable.com>
2023-01-31 14:51:55 +1000

    DX-1684: Added/Updated metadata for Nuget Packages

--- publish-to-nuget.sh
+++ publish-to-nuget.sh
@@ -5,5 +5,13 @@ OUT_DIR="NugetPackages"
 API_KEY="" # Get API Key from your Nuget Account
 cd ${OUT_DIR}
 
+# Publishes locally built packages to Nuget https://www.nuget.org/account/Packages
+
 dotnet nuget push Imx.Sdk.${VERSION_STR}.nupkg --api-key ${API_KEY} --source https://api.nuget.org/v3/index.json
 dotnet nuget push Imx.Sdk.Gen.${VERSION_STR}.nupkg --api-key ${API_KEY} --source https://api.nuget.org/v3/index.json
+
+
+## For Deleting NUget Packages
+
+#dotnet nuget delete Imx.Sdk ${VERSION_STR} --api-key ${API_KEY} --source https://api.nuget.org/v3/index.json
+#dotnet nuget delete Imx.Sdk.Gen ${VERSION_STR} --api-key ${API_KEY} --source https://api.nuget.org/v3/index.json
