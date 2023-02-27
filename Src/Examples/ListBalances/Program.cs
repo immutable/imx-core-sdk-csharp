9a91552f74d0f24e044487d62a76dc9ed852f7e9
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2023-02-27 13:00:07 +1300

    update GetBalances example
    Co-authored-by: Praveen Suluvai <suluvai@users.noreply.github.com>
    Co-authored-by: Carl Menezes <carleeto@users.noreply.github.com>

--- Src/Examples/ListBalances/Program.cs
+++ Src/Examples/ListBalances/Program.cs
@@ -3,13 +3,9 @@ using Imx.Sdk.Gen.Model;
 using Imx.Sdk;
 
 // Address of the owner/user
-var ownerToken = "0x2d0ad946788938B9044ed72b1C464e1e9bb9d401"; // Swap this out for your own owner token address
-
-// Token address of 'eth'
-var ethTokenAddress = "eth";
-
-// Token address of Gods Unchained
-var guTokenAddress = "0x5c9f1680bb6a4b4fc698e0cf702e0cc34aed91b7";
+var ownerAddress = "0x2d0ad946788938B9044ed72b1C464e1e9bb9d401"; // Swap this out for your own address
+// Token address for 'eth'
+var tokenAddress = "eth"; // Only for the token eth we pass in the string 'eth' for all other token types need to pass in the token address like '0x5c9f1680bb6a4b4fc698e0cf702e0cc34aed91b7' (this is GODS token address)
 
 Client client = new Client(new Config()
 {
@@ -19,21 +15,8 @@ Client client = new Client(new Config()
 // Get ETH balance for owner token example
 try
 {
-    var balance = client.GetBalance(ownerToken, ethTokenAddress);
-    Console.WriteLine($"ETH balance for owner token {ownerToken}: " + balance.ToJson());
-}
-catch (ApiException e)
-{
-    Console.WriteLine("Exception when calling GetBalance: " + e.Message);
-    Console.WriteLine("Status Code: " + e.ErrorCode);
-    Console.WriteLine(e.StackTrace);
-}
-
-// Get ERC721 token balance for Gods Unchained
-try
-{
-    var balance = client.GetBalance(ownerToken, guTokenAddress);
-    Console.WriteLine($"ETH balance for owner token {ownerToken}: " + balance.ToJson());
+    var balance = client.GetBalance(ownerAddress, tokenAddress);
+    Console.WriteLine($"ETH balance for owner token {ownerAddress}: " + balance.ToJson());
 }
 catch (ApiException e)
 {
@@ -45,8 +28,8 @@ catch (ApiException e)
 // List balances for owner token example
 try
 {
-    var balances = client.ListBalances(ownerToken);
-    Console.WriteLine($"List of balances for owner token {ownerToken}: " + balances.ToJson());
+    var balances = client.ListBalances(ownerAddress);
+    Console.WriteLine($"List of balances for owner token {ownerAddress}: " + balances.ToJson());
 }
 catch (ApiException e)
 {
